aec59cd175b4649795f5c6633ce846a77c6f1260
MG177 <mg.terok77@gmail.com>
2022-12-23 17:56:07 +0800

    fix unsupported password test object

--- Object Repository/Auth/Register/p_Password unsupported.rs
+++ Object Repository/Auth/Register/p_Password unsupported.rs
@@ -6,16 +6,16 @@
    <elementGuidId>0852e1df-d61c-4144-a073-732a23203a84</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>CSS</key>
-         <value>div.input_error.mb-1 > p</value>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div[2]/div/form/div[4]/p</value>
       </entry>
       <entry>
          <key>BASIC</key>
-         <value>//p[(contains(text(), ' Gunakan huruf kapital, huruf kecil dan angka numerik') or contains(., ' Gunakan huruf kapital, huruf kecil dan angka numerik'))]</value>
+         <value>//p[(contains(text(), ' Gunakan kombinasi huruf kapital, huruf kecil dan angka numerik') or contains(., ' Gunakan kombinasi huruf kapital, huruf kecil dan angka numerik'))]</value>
       </entry>
       <entry>
-         <key>XPATH</key>
-         <value>//div[@id='root']/div[2]/div/form/div[4]/p</value>
+         <key>CSS</key>
+         <value>div.input_error.mb-1 > p</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -26,15 +26,15 @@
       <name>tag</name>
       <type>Main</type>
       <value>p</value>
-      <webElementGuid>15062c82-306e-4ce5-9744-a01399910803</webElementGuid>
+      <webElementGuid>2e6e2828-1d5b-4d99-beeb-650cd208ab75</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>contains</matchCondition>
       <name>text</name>
       <type>Main</type>
-      <value> Gunakan huruf kapital, huruf kecil dan angka numerik</value>
-      <webElementGuid>ccd46762-0e04-4099-afda-5c93df236b21</webElementGuid>
+      <value> Gunakan kombinasi huruf kapital, huruf kecil dan angka numerik</value>
+      <webElementGuid>c09f45fa-e161-40ef-923b-9e5938d1d415</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -42,7 +42,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;root&quot;)/div[@class=&quot;Wrap&quot;]/div[@class=&quot;containerLogin d-flex flex-column justify-content-center&quot;]/form[1]/div[@class=&quot;input_error mb-1&quot;]/p[1]</value>
-      <webElementGuid>089851ee-f372-4734-a6a0-129e0d2ae1df</webElementGuid>
+      <webElementGuid>4b011503-aa52-486b-a93a-96191c0ca926</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -50,7 +50,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='root']/div[2]/div/form/div[4]/p</value>
-      <webElementGuid>f26a9c86-61ca-4fdd-b1df-f53869b373a5</webElementGuid>
+      <webElementGuid>3480fc56-1968-427f-8d12-afd06b72af1b</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -58,7 +58,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Ginap'])[1]/following::p[3]</value>
-      <webElementGuid>c53d3349-36d7-4e92-be2e-012df8a0f3e5</webElementGuid>
+      <webElementGuid>cd965ca7-1233-4916-84a3-9a019ed2594a</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -66,7 +66,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Kata sandi minimal 8 karakter. Gunakan huruf kapital, huruf kecil dan angka numerik.'])[1]/preceding::p[1]</value>
-      <webElementGuid>01cbfc39-4e4b-45d2-b961-835b70a3e6db</webElementGuid>
+      <webElementGuid>f4b36ee2-e640-46a8-959b-a8d667d3eb44</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -74,7 +74,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Berikutnya'])[1]/preceding::p[1]</value>
-      <webElementGuid>e91b9032-bdc1-420e-af12-ee54294904f6</webElementGuid>
+      <webElementGuid>b7470fef-1dca-4687-8ddb-959a0679a978</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -82,7 +82,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[4]/p</value>
-      <webElementGuid>9d2b4450-5c72-479f-ac88-9145a163dbbc</webElementGuid>
+      <webElementGuid>6b7d0530-e32c-4ef5-a3fb-6262fda94723</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -90,6 +90,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//p[(text() = ' This field is required!' or . = ' This field is required!')]</value>
-      <webElementGuid>c75dd0bf-3dba-43b1-b563-77d4e67d7425</webElementGuid>
+      <webElementGuid>b2ab71fa-e28b-43ba-b1cc-6d0df69c94d6</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
